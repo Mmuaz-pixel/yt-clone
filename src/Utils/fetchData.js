e454f76ddc6a8d2166e6c3d19b6773387c000885
Mmuaz-pixel <116841220+Mmuaz-pixel@users.noreply.github.com>
2023-09-03 23:12:10 +0500

    Update fetchData.js

--- src/Utils/fetchData.js
+++ src/Utils/fetchData.js
@@ -1,22 +1,4 @@
 const CONSTANT_URL = 'https://youtube-v31.p.rapidapi.com'; 
-
-// const apis = ['46f6d67d0fmshb031d0781f4a43bp15b18cjsne9c1d15ab5e4', '9cc91d7742mshe015120a23ac8b4p13e8d7jsn4a1d23de0b00']
-
-// // Generate a random floating-point number between 0 (inclusive) and 1 (exclusive)
-// const randomFloat = Math.random();
-// console.log("Random Float:", randomFloat);
-
-// // Generate a random integer between min (inclusive) and max (exclusive)
-// function getRandomInt(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min)) + min;
-// }
-
-// const min = 0; 
-// const max = 2; 
-// const randomInt = getRandomInt(min, max);
-
 const api = ""; 
 
 const options = {
